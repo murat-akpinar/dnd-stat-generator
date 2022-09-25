6899df6fb91bcc229d587373976c85b3bcdc3ba9
Murat Akpınar <112278587+Shyuuhei@users.noreply.github.com>
2022-09-25 21:03:55 +0300

    Update DndStatGeneration.sh

--- DndStatGeneration.sh
+++ DndStatGeneration.sh
@@ -5,23 +5,23 @@ liste=()
 
 for (( count=0; count<4; count++  ))
 do
-	zar=$(( (RANDOM %6) +1 ))
-	liste+=($zar)
-	
+        zar=$(( (RANDOM %6) +1 ))
+        liste+=($zar)
+
 done
 for (( count=0; count<4; count++  ))
 do
-	echo -n "${liste[$count]} "
+        echo -n "${liste[$count]} "
 done
 
-echo 
 
 IFS=$'\n' sorted=($(sort <<<"${liste[*]}"))
 unset IFS
 
 let toplam=${sorted[1]}+${sorted[2]}+${sorted[3]}
-echo "Toplam(4d6kh3)"  $toplam
 
+echo " = Toplam(4d6kh3):" $toplam
+echo -----------------------
 }
 
 sonuc=0
@@ -29,7 +29,8 @@ sonuc=0
 
 for (( countt=0; countt<6; countt++ ))
 do
-	dndfunction
-	let sonuc+=$toplam
+        dndfunction
+        let sonuc+=$toplam
 done
-echo geneltoplam $sonuc
+
+echo GENEL TOPLAM : $sonuc
